3b08a6b11e07282455256141301b82b558a8ab1d
Gabriel Turcu <turcu.gabriel93@gmail.com>
2024-09-22 15:54:07 +0300

    update addToCart tests

--- cypress/e2e/generalTests/addToCart.cy.js
+++ cypress/e2e/generalTests/addToCart.cy.js
@@ -6,25 +6,25 @@ describe('Login Tests', () => {
         cy.visit("/");
     })
 
-    // it("Add a product in the cart", () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="inventory-item"]').first().find('button').click(); // click add to cart button
-    //     cy.get('[data-test="shopping-cart-badge"]').should('contain.text', '1'); // check if the cart badge have a text
-    //     cy.get('[data-test="inventory-item"]').first().find('button').should('have.text', 'Remove'); // check if the add to cart button text was changed
-    //     cy.get('[data-test="shopping-cart-link"]').click(); // go to cart
-    //     cy.get('.cart_item').should('have.length', 1); // check if cart contains an item
-    // })
+    it("Add a product in the cart", () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="inventory-item"]').first().find('button').click(); // click add to cart button
+        cy.get('[data-test="shopping-cart-badge"]').should('contain.text', '1'); // check if the cart badge have a text
+        cy.get('[data-test="inventory-item"]').first().find('button').should('have.text', 'Remove'); // check if the add to cart button text was changed
+        cy.get('[data-test="shopping-cart-link"]').click(); // go to cart
+        cy.get('.cart_item').should('have.length', 1); // check if cart contains an item
+    })
 
 
-    // it("Delete a product in the cart", () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="inventory-item"]').first().find('button').click(); // click add to cart button
-    //     cy.get('[data-test="shopping-cart-link"]').click(); // go to cart
-    //     cy.get('.cart_item').should('have.length', 1); // check if cart contains an item
-    //     cy.get('.cart_item').first().find('button').click(); // find and hit the remove button
-    //     cy.get('[data-test="shopping-cart-badge"]').should('not.exist'); // check if the cart badge exist
-    //     cy.get('.cart_item').should('not.exist'); // check if the cart is empty
-    // })
+    it("Delete a product in the cart", () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="inventory-item"]').first().find('button').click(); // click add to cart button
+        cy.get('[data-test="shopping-cart-link"]').click(); // go to cart
+        cy.get('.cart_item').should('have.length', 1); // check if cart contains an item
+        cy.get('.cart_item').first().find('button').click(); // find and hit the remove button
+        cy.get('[data-test="shopping-cart-badge"]').should('not.exist'); // check if the cart badge exist
+        cy.get('.cart_item').should('not.exist'); // check if the cart is empty
+    })
 
     it("Add Multiple Products to Cart", () => {
         login(info.correctCredentials.user, info.correctCredentials.password); // login function
--- cypress/e2e/generalTests/productPage.cy.js
+++ cypress/e2e/generalTests/productPage.cy.js
@@ -6,48 +6,48 @@ describe('Login Tests', () => {
         cy.visit("/");
     })
 
-    // it("Access the product page", () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="inventory-item-name"]').first().click(); // access the first product in the list
-    //     cy.get('[data-test="back-to-products"]').should('exist'); // check if the back to product list button exist
-    // })
+    it("Access the product page", () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="inventory-item-name"]').first().click(); // access the first product in the list
+        cy.get('[data-test="back-to-products"]').should('exist'); // check if the back to product list button exist
+    })
 
-    // it("Go back to product page", () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="inventory-item-name"]').first().click(); // access the first product in the list
-    //     cy.get('[data-test="back-to-products"]').click(); // hit the back to product listing button
-    //     cy.get('[data-test="title"]').should('exist'); // check if the title page exist
-    // })
+    it("Go back to product page", () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="inventory-item-name"]').first().click(); // access the first product in the list
+        cy.get('[data-test="back-to-products"]').click(); // hit the back to product listing button
+        cy.get('[data-test="title"]').should('exist'); // check if the title page exist
+    })
 
-    // it("Check if the hamburger menu is woring", () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('#react-burger-menu-btn').click({force: true}); // open menu
-    //     cy.get('#react-burger-cross-btn').click(); // close menu
-    //     cy.get('[data-test="close-menu"]').should('not.visible'); // check if the close menu button is visible
-    // })
+    it("Check if the hamburger menu is woring", () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('#react-burger-menu-btn').click({force: true}); // open menu
+        cy.get('#react-burger-cross-btn').click(); // close menu
+        cy.get('[data-test="close-menu"]').should('not.visible'); // check if the close menu button is visible
+    })
 
-    // it('Product sorting - A to Z', () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="product-sort-container"]').select('Name (A to Z)'); // select sorting field
-    //     cy.get('[data-test="inventory-item-name"]').then((productNames) => {  // verify that the products are sorted alphabetically from A to Z
-    //         const actualProductNames = [...productNames].map(name => name.innerText);
-    //         const sortedProductNames = [...actualProductNames].sort(); // sort array alphabetically A to Z
-    //         expect(actualProductNames).to.deep.equal(sortedProductNames); // check if the actual order matches the sorted order
-    //     });
-    //     cy.get('[data-test="inventory-item"]').should('have.length', 6); // assuming there are 6 products
-    // })
+    it('Product sorting - A to Z', () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="product-sort-container"]').select('Name (A to Z)'); // select sorting field
+        cy.get('[data-test="inventory-item-name"]').then((productNames) => {  // verify that the products are sorted alphabetically from A to Z
+            const actualProductNames = [...productNames].map(name => name.innerText);
+            const sortedProductNames = [...actualProductNames].sort(); // sort array alphabetically A to Z
+            expect(actualProductNames).to.deep.equal(sortedProductNames); // check if the actual order matches the sorted order
+        });
+        cy.get('[data-test="inventory-item"]').should('have.length', 6); // assuming there are 6 products
+    })
 
 
-    // it('Product sorting - Z to A', () => {
-    //     login(info.correctCredentials.user, info.correctCredentials.password); // login function
-    //     cy.get('[data-test="product-sort-container"]').select('Name (Z to A)'); // select sorting field
-    //     cy.get('[data-test="inventory-item-name"]').then((productNames) => {  // verify that the products are sorted alphabetically from A to Z
-    //         const actualProductNames = [...productNames].map(name => name.innerText);
-    //         const sortedProductNames = [...actualProductNames].sort().reverse(); // sort array alphabetically Z to A
-    //         expect(actualProductNames).to.deep.equal(sortedProductNames); // check if the actual order matches the sorted order
-    //     });
-    //     cy.get('[data-test="inventory-item"]').should('have.length', 6); // assuming there are 6 products
-    // })
+    it('Product sorting - Z to A', () => {
+        login(info.correctCredentials.user, info.correctCredentials.password); // login function
+        cy.get('[data-test="product-sort-container"]').select('Name (Z to A)'); // select sorting field
+        cy.get('[data-test="inventory-item-name"]').then((productNames) => {  // verify that the products are sorted alphabetically from A to Z
+            const actualProductNames = [...productNames].map(name => name.innerText);
+            const sortedProductNames = [...actualProductNames].sort().reverse(); // sort array alphabetically Z to A
+            expect(actualProductNames).to.deep.equal(sortedProductNames); // check if the actual order matches the sorted order
+        });
+        cy.get('[data-test="inventory-item"]').should('have.length', 6); // assuming there are 6 products
+    })
 
 
     it('Product sorting - Price (Low to High)', () => {
